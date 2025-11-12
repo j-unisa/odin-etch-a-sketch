31db7b21548e8e764a839887ad1a1cdb0ad273cb
j-unisa <104635026+j-unisa@users.noreply.github.com>
2025-11-12 10:39:29 -0800

    feat: add class to grid squares
    - Add "grid-square" class to divs created dynamically in #grid-container

--- js/script.js
+++ js/script.js
@@ -3,5 +3,6 @@ const gridContainer = document.querySelector("#grid-container");
 for (let i = 0; i < 16; i++)
 {
     let gridSquare = document.createElement("div");
+    gridSquare.classList.toggle("grid-square");
     gridContainer.appendChild(gridSquare);
 }
